96ca1f7190581714d2ea651913a606334847c6b2
Kazi Jamil <jrobo786@outlook.com>
2024-04-11 17:00:43 +0200

    test run after modification to see the plot

--- train.sh
+++ train.sh
@@ -2,11 +2,20 @@
 
 # Command 1
 
-echo "Training with sigma=0.04"
-python3 ema_train_deep_ppca.py --sigma 0.04 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+echo "Training with sigma=0.001"
+python3 ema_train_deep_ppca.py --sigma 0.001 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
 
 # Command 2
+echo "Training with sigma=0.00"
+python3 ema_train_deep_ppca.py --sigma 0.01 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+
+
+# Command 3
+echo "Training with sigma=0.04"
+python3 ema_train_deep_ppca.py --sigma 0.04 --lr 0.0001 --num_epochs 30000 --s>
+
+# Command 4
 echo "Training with sigma=0.06"
-python3 ema_train_deep_ppca.py --sigma 0.06 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+python3 ema_train_deep_ppca.py --sigma 0.06 --lr 0.0001 --num_epochs 30000 --s>
 
 
--- train2.sh
+++ train2.sh
@@ -1,8 +1,19 @@
-# Command  3
-echo "Training with sigma=0.4"
-python3 ema_train_deep_ppca.py --sigma 0.4 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+# Command 5
 
-# Command 4
+echo "Training with sigma=0.1"
+python3 ema_train_deep_ppca.py --sigma 0.1 --lr 0.0001 --num_epochs 30000 --s>
+
+# Command 6
+echo "Training with sigma=0.4"
+python3 ema_train_deep_ppca.py --sigma 0.4 --lr 0.0001 --num_epochs 30000 --s>
+
+# Command  7
 echo "Training with sigma=0.6"
 python3 ema_train_deep_ppca.py --sigma 0.6 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+
+# Command 8
+echo "Training with sigma=1"
+python3 ema_train_deep_ppca.py --sigma 1 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+
+
 
