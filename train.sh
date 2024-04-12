a8b20e1368cfceda4315720cef4755a251fd9666
Kazi Jamil <jrobo786@outlook.com>
2024-04-12 14:02:00 +0200

    new train with modified code

--- train.sh
+++ train.sh
@@ -2,12 +2,12 @@
 
 # Command 1
 
-echo "Training with sigma=0.001"
-python3 ema_train_deep_ppca.py --sigma 0.001 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+#echo "Training with sigma=0.001"
+#python3 ema_train_deep_ppca.py --sigma 0.001 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
 
 # Command 2
-echo "Training with sigma=0.00"
-python3 ema_train_deep_ppca.py --sigma 0.01 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+#echo "Training with sigma=0.00"
+#python3 ema_train_deep_ppca.py --sigma 0.01 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
 
 
 # Command 3
--- train2.sh
+++ train2.sh
@@ -1,19 +1,16 @@
 # Command 5
-
 echo "Training with sigma=0.1"
-python3 ema_train_deep_ppca.py --sigma 0.1 --lr 0.0001 --num_epochs 30000 --s>
+python3 ema_train_deep_ppca.py --sigma 0.1 --lr 0.0001 --num_epochs 30000
 
 # Command 6
 echo "Training with sigma=0.4"
-python3 ema_train_deep_ppca.py --sigma 0.4 --lr 0.0001 --num_epochs 30000 --s>
+python3 ema_train_deep_ppca.py --sigma 0.4 --lr 0.0001 --num_epochs 30000
 
-# Command  7
+# Command 7
 echo "Training with sigma=0.6"
-python3 ema_train_deep_ppca.py --sigma 0.6 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
+python3 ema_train_deep_ppca.py --sigma 0.6 --lr 0.0001 --num_epochs 30000
 
 # Command 8
 echo "Training with sigma=1"
-python3 ema_train_deep_ppca.py --sigma 1 --lr 0.0001 --num_epochs 30000 --seed 123 --ema_alpha 0.01
-
-
+python3 ema_train_deep_ppca.py --sigma 1 --lr 0.0001 --num_epochs 30000
 
